98f02a47f2b0a2c72efbb317d6bb5284f908a50d
--global <--global>
2023-04-25 23:29:13 +0300

    commit

--- todo/todo.js
+++ todo/todo.js
@@ -2,7 +2,14 @@
 
 const insertBtn = document.querySelector("#insert-btn");
 const input = document.querySelector('#task')
-
+const Toggle = document.createElement("button")
+Toggle.type="button"
+Toggle.innerHTML='Toggle'
+document.body.append(Toggle)
+let color = 0
+Toggle.addEventListener('click',(e) => {
+color
+})
 const list = []
 
 const renderList = () => {
@@ -10,9 +17,9 @@ const renderList = () => {
     listElem.innerHTML = '';
     list.forEach(listItem => {
         const li = document.createElement('li');
+        li.style.background="gray";
         li.innerText = listItem.value;
         li.id = listItem.id;
-
         listElem.append(li);
     });
 }
